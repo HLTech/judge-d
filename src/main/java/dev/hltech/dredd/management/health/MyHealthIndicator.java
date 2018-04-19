13c35bee66308a7629572e54e899cfcc1a8a7ce8
Tomasz Krzyżak <tomasz.krzyzak@hl.co.uk>
2018-04-19 09:32:22 +0100

    review remarks

--- src/main/java/dev/hltech/dredd/management/health/MyHealthIndicator.java
+++ src/main/java/dev/hltech/dredd/management/health/MyHealthIndicator.java
@@ -9,21 +9,10 @@ public class MyHealthIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        int errorCode = check();
-        if (errorCode != 0) {
-            return Health
-                .down()
-                .withDetail("Error Code", errorCode)
-                .build();
-        }
         return Health
             .up()
             .withDetail("foo", "bar")
             .build();
     }
 
-    private int check() {
-        // perform some specific health check
-        return 0;
-    }
 }
