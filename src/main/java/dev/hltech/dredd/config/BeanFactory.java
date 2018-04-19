9b0d10ba8c904f143e3b93e21d88c0b0e306d2ea
Tomasz Krzyżak <tomasz.krzyzak@hl.co.uk>
2018-04-19 21:02:02 +0100

    delegate validation of a swagger to SwaggerValidator

--- src/main/java/dev/hltech/dredd/config/BeanFactory.java
+++ src/main/java/dev/hltech/dredd/config/BeanFactory.java
@@ -1,6 +1,8 @@
 package dev.hltech.dredd.config;
 
+import au.com.dius.pact.model.RequestResponsePact;
 import dev.hltech.dredd.domain.PactValidator;
+import dev.hltech.dredd.domain.SwaggerValidator;
 import dev.hltech.dredd.domain.environment.Environment;
 import dev.hltech.dredd.domain.environment.StaticEnvironment;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
+import static au.com.dius.pact.model.PactReader.loadPact;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.io.ByteStreams.toByteArray;
 
 @Configuration
@@ -21,12 +25,24 @@ public class BeanFactory {
                 "1.0",
                 new String(toByteArray(getClass().getResourceAsStream("/dde-instruction-gateway-swagger.json")))
             )
+            .withConsumer(
+                "frontend",
+                "1.0",
+                newArrayList(
+                    (RequestResponsePact) loadPact(getClass().getResourceAsStream("/pact-frontend-to-dde-instruction-gateway.json"))
+                )
+            )
             .build();
     }
 
     @Bean
     public PactValidator pactValidator(Environment environment){
         return new PactValidator(environment);
+    }
+
+    @Bean
+    public SwaggerValidator swaggerValidator(Environment environment) {
+        return new SwaggerValidator(environment);
     }
 
 }
