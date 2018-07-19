47f2d6f4cd724bd480ea3a09ec665dce7d9a0e6a
Tomasz Krzyżak <tomasz.krzyzak@hltech.com>
2018-07-19 23:43:27 +0200

    separate judge-d REST model from servicelocator model

--- judge-d-agent/src/main/java/com/hltech/contracts/judged/agent/ServiceLocator.java
+++ judge-d-agent/src/main/java/com/hltech/contracts/judged/agent/ServiceLocator.java
@@ -1,9 +1,25 @@
 package com.hltech.contracts.judged.agent;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.Set;
 
 public interface ServiceLocator {
 
-    Set<JudgeDPublisher.ServiceForm> locateServices();
+    Set<Service> locateServices();
+
+    @Getter
+    @AllArgsConstructor
+    @ToString
+    @EqualsAndHashCode
+    class Service {
+
+        private String name;
+        private String version;
+
+    }
 
 }
