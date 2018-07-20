fbc1e132a364bc54d184c884410c2235d8d3c142
Tomasz Krzyżak <tomasz.krzyzak@hltech.com>
2018-07-20 17:10:10 +0200

    move rest validation logic to proper package

--- judge-d-server/src/main/java/dev/hltech/dredd/domain/validation/rest/PactExpectationsValidator.java
+++ judge-d-server/src/main/java/dev/hltech/dredd/domain/validation/rest/PactExpectationsValidator.java
@@ -1,18 +1,26 @@
 package dev.hltech.dredd.domain.validation.rest;
 
+import au.com.dius.pact.model.RequestResponsePact;
 import dev.hltech.dredd.domain.validation.ExpectationValidationResult;
 import dev.hltech.dredd.domain.contracts.ServiceContracts;
 import dev.hltech.dredd.domain.validation.ExpectationsValidator;
 
-public class PactExpectationsValidator implements ExpectationsValidator {
+import static au.com.dius.pact.model.PactReader.loadPact;
 
+public class PactExpectationsValidator extends RestContractValidator implements ExpectationsValidator {
 
-    public PactExpectationsValidator(String expectations) {
-        // parse pact file
+    private final RequestResponsePact requestResponsePact;
+
+    public PactExpectationsValidator(String pact) {
+        requestResponsePact = (RequestResponsePact) loadPact(pact);
     }
 
     @Override
     public ExpectationValidationResult validate(ServiceContracts serviceContracts) {
-        return null;
+        return new ExpectationValidationResult(
+            serviceContracts.getName(),
+            serviceContracts.getVersion(),
+            validate(requestResponsePact, serviceContracts.getCapabilities(RestContractValidatorFactory.COMMUNICATION_INTERFACE).get())
+        );
     }
 }
