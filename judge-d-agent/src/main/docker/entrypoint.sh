1908ae21b8c4a87d8c67a9014fb6de6955123f9b
Tomasz Krzyżak <tomasz.krzyzak@hltech.com>
2018-09-28 21:11:56 +0200

    improve memory usage when running in docker image

--- judge-d-agent/src/main/docker/entrypoint.sh
+++ judge-d-agent/src/main/docker/entrypoint.sh
@@ -2,4 +2,6 @@
 java \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
+-XX:MaxRAMFraction=1 \
+-XshowSettings:vm \
 $* -jar judge-d-agent.jar
--- judge-d-server/src/main/docker/entrypoint.sh
+++ judge-d-server/src/main/docker/entrypoint.sh
@@ -2,4 +2,7 @@
 java \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
+-XX:+UseCGroupMemoryLimitForHeap \
+-XX:MaxRAMFraction=1 \
+-XshowSettings:vm \
 $* -jar judge-d.jar
