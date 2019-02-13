d164642c3a69cfca21c5c528dd5bbce77eb2ca71
Filip Łazarski <filip.lazarski@hltech.com>
2019-02-13 12:58:47 +0100

    EnableScheduling annotation added

--- judge-d-agent/src/main/java/com/hltech/contracts/judged/agent/config/BeanFactory.java
+++ judge-d-agent/src/main/java/com/hltech/contracts/judged/agent/config/BeanFactory.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
@@ -30,6 +31,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 
 @Configuration
+@EnableScheduling
 public class BeanFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BeanFactory.class);
