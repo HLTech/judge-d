6fef526512bed1b90caa211bfe6d17ebdb942f40
Filip Łazarski <filip.lazarski@hltech.com>
2019-06-06 16:27:13 +0200

    VauntSerializer introduced

--- judge-d-server/src/main/java/dev/hltech/dredd/domain/validation/jms/JmsContractValidator.java
+++ judge-d-server/src/main/java/dev/hltech/dredd/domain/validation/jms/JmsContractValidator.java
@@ -1,9 +1,6 @@
 package dev.hltech.dredd.domain.validation.jms;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.hltech.vaunt.core.VauntSerializer;
 import com.hltech.vaunt.core.domain.model.Contract;
 import com.hltech.vaunt.validator.ValidationError;
 import com.hltech.vaunt.validator.ValidationResult;
@@ -12,8 +9,6 @@ import dev.hltech.dredd.domain.validation.InterfaceContractValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,35 +18,18 @@ public class JmsContractValidator extends InterfaceContractValidator<List<Contra
 
     public static final String COMMUNICATION_INTERFACE = "jms";
 
-    private final ObjectMapper objectMapper;
-
     public JmsContractValidator() {
         super(COMMUNICATION_INTERFACE);
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new GuavaModule());
-        objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Override
     public List<Contract> asCapabilities(String rawCapabilities) {
-        try {
-            return objectMapper.readValue(rawCapabilities, new TypeReference<List<Contract>>(){});
-        } catch (IOException e) {
-            //TODO: after importing vaunt, provide handling
-            log.error(e.getMessage());
-        }
-        return new ArrayList<>();
+        return new VauntSerializer().parseContracts(rawCapabilities);
     }
 
     @Override
     public List<Contract> asExpectations(String rawExpectations) {
-        try {
-            return objectMapper.readValue(rawExpectations, new TypeReference<List<Contract>>(){});
-        } catch (IOException e) {
-            //TODO: after importing vaunt, provide handling
-            log.error(e.getMessage());
-        }
-        return new ArrayList<>();
+        return new VauntSerializer().parseContracts(rawExpectations);
     }
 
     @Override
