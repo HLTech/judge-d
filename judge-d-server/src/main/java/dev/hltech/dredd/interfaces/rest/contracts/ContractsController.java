452c827fcfd4f5c9e86a7749ab8afab6f73ad4a9
Karol Kalinski <karol.kalinski@hltech.com>
2019-08-06 15:12:59 +0200

    endpoints recovered to guarantee gradual rollout of API changed

--- judge-d-server/src/main/java/dev/hltech/dredd/interfaces/rest/contracts/ContractsController.java
+++ judge-d-server/src/main/java/dev/hltech/dredd/interfaces/rest/contracts/ContractsController.java
@@ -9,15 +9,19 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.persistence.EntityNotFoundException;
 import javax.persistence.NoResultException;
 import java.util.List;
-import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
@@ -54,6 +58,13 @@ public class ContractsController {
         return serviceContractsRepository.getServiceNames();
     }
 
+    /**
+     * This endpoint is intended to server request checking if a service with given registered any contracts.
+     * It will be extended to provide more detailed information on a service in the future.
+     *
+     * @param serviceName - name of the service that we are querying for
+     * @return name of the service if contracts are present, NOT_FOUND (404) otherwise
+     */
     @GetMapping(value = "/services/{serviceName}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Get details of a services with registered contracts", nickname = "get service details")
     @ApiResponses(value = {
@@ -67,9 +78,7 @@ public class ContractsController {
 
     @ExceptionHandler(NoResultException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    void notFound() {
-
-    }
+    void notFound() { }
 
     @GetMapping(value = "/services/{serviceName}/versions", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Get versions of a service with registered contracts", nickname = "get versions of a service")
@@ -77,7 +86,7 @@ public class ContractsController {
         @ApiResponse(code = 200, message = "Success", response = String.class, responseContainer = "list"),
         @ApiResponse(code = 400, message = "Bad Request"),
         @ApiResponse(code = 500, message = "Failure")})
-    public List<String> getServiceVersions(@PathVariable(name = "serviceName") String serviceName) {
+    public List<String> getAllServiceVersions(@PathVariable(name = "serviceName") String serviceName) {
         return serviceContractsRepository.find(serviceName)
             .stream()
             .map(ServiceContracts::getVersion).sorted()
@@ -90,7 +99,7 @@ public class ContractsController {
         @ApiResponse(code = 200, message = "Success", response = ServiceContractsDto.class),
         @ApiResponse(code = 400, message = "Bad Request"),
         @ApiResponse(code = 500, message = "Failure")})
-    public ServiceContractsDto get(@PathVariable(name = "serviceName") String serviceName, @PathVariable(name = "version") String version) {
+    public ServiceContractsDto getContracts(@PathVariable(name = "serviceName") String serviceName, @PathVariable(name = "version") String version) {
         return mapper.toDto(this.serviceContractsRepository.find(serviceName, version).orElseThrow(ResourceNotFoundException::new));
     }
 
@@ -100,7 +109,7 @@ public class ContractsController {
         @ApiResponse(code = 200, message = "Success", response = ServiceContractsDto.class),
         @ApiResponse(code = 400, message = "Bad Request"),
         @ApiResponse(code = 500, message = "Failure")})
-    public ServiceContractsDto create(@PathVariable(name = "serviceName") String serviceName, @PathVariable(name = "version") String version, @RequestBody ServiceContractsForm form) {
+    public ServiceContractsDto registerContract(@PathVariable(name = "serviceName") String serviceName, @PathVariable(name = "version") String version, @RequestBody ServiceContractsForm form) {
         return mapper.toDto(this.serviceContractsRepository.persist(
             new ServiceContracts(
                 serviceName,
@@ -110,4 +119,48 @@ public class ContractsController {
             )
         ));
     }
+
+    @PostMapping(value = "/{provider}/{version:.+}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Register contracts for a version of a service", nickname = "register contracts")
+    @Deprecated
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success", response = ServiceContractsDto.class),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 500, message = "Failure")})
+    public ServiceContractsDto create(@PathVariable(name = "provider") String provider, @PathVariable(name = "version") String version, @RequestBody ServiceContractsForm form) {
+        return mapper.toDto(this.serviceContractsRepository.persist(
+            new ServiceContracts(
+                provider,
+                version,
+                mapper.mapCapabilitiesForm(form.getCapabilities()),
+                mapper.mapExpectationsForm(form.getExpectations())
+            )
+        ));
+    }
+
+    @GetMapping(value = "/{serviceName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get versions of a service with registered contracts", nickname = "get versions of a service")
+    @Deprecated
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success", response = String.class, responseContainer = "list"),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 500, message = "Failure")})
+    public List<String> getServiceVersions(@PathVariable(name = "serviceName") String serviceName) {
+        return serviceContractsRepository.find(serviceName)
+            .stream()
+            .map(ServiceContracts::getVersion).sorted()
+            .collect(toList());
+    }
+
+    @GetMapping(value = "/{provider}/{version:.+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get contracts for a version of a service", nickname = "get contracts")
+    @Deprecated
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success", response = ServiceContractsDto.class),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 500, message = "Failure")})
+    public ServiceContractsDto get(@PathVariable(name = "provider") String provider, @PathVariable(name = "version") String version) {
+        return mapper.toDto(this.serviceContractsRepository.find(provider, version).orElseThrow(ResourceNotFoundException::new));
+    }
+
 }
