a778144717ba0232a74bb1192d9e51f98701f2af
Tomasz Krzyżak <krzyzak@accuratus-software.pl>
2019-12-12 15:08:02 +0100

    add @ToString to ServiceVersion

--- judge-d-server/src/main/java/dev/hltech/dredd/domain/ServiceVersion.java
+++ judge-d-server/src/main/java/dev/hltech/dredd/domain/ServiceVersion.java
@@ -1,10 +1,7 @@
 package dev.hltech.dredd.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
@@ -15,6 +12,7 @@ import java.io.Serializable;
 import static lombok.AccessLevel.PROTECTED;
 
 @Embeddable
+@ToString
 @Getter
 @EqualsAndHashCode
 @AllArgsConstructor
