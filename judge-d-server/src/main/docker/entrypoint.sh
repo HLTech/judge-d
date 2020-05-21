1824122afaa23b7254c778a28db6a71fb8fc927e
Filip Łazarski <filip.lazarski@hltech.com>
2020-05-21 14:45:10 +0200

    Adjusting memory options for JDK 11 and entrypoint script

--- judge-d-server/src/main/docker/entrypoint.sh
+++ judge-d-server/src/main/docker/entrypoint.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-MEMORY_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=2 -XshowSettings:vm "
-JAVA_OPTIONS=$MEMORY_OPTIONS
-
-if [[ -v VALIDATION_OPTIONS ]]; then
-    echo "Selected validation options: $VALIDATION_OPTIONS"
-    validation_options_array=(${VALIDATION_OPTIONS//,/ })
-    validation_options_array_length=${#validation_options_array[@]}
-    for (( i=0; i<${validation_options_array_length}; i++ ));
-    do
-        validation_options_array[$i]="-Dswagger."${validation_options_array[$i]}
-        JAVA_OPTIONS+="${validation_options_array[$i]} "
+if [[ -z "$VALIDATION_OPTIONS" ]]; then
+    echo "Selected validation options: ${VALIDATION_OPTIONS}"
+    validation_options_array=`echo ${VALIDATION_OPTIONS} | tr ',' ' '`
+    for i in ${validation_options_array}; do
+      SWAGGER_OPTIONS+=" -Dswagger."$i
     done
 fi
 
 set -x
-java $JAVA_OPTIONS -jar judge-d.jar
+java ${SWAGGER_OPTIONS} -jar judge-d.jar
