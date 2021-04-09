c1ff14a8ce9ad50b52456e6edc52f4cc3d454b85
Adam Kolodziejczyk <Adam.Kolodziejczyk@hltech.com>
2021-04-09 14:32:53 +0200

    Add ValidationControllerFT, unify service versions in FT

--- judge-d-server/src/test/resources/com/hltech/judged/server/ContractsControllerFT.GetVersions.sql
+++ judge-d-server/src/test/resources/com/hltech/judged/server/ContractsControllerFT.GetVersions.sql
@@ -1,4 +1,4 @@
-INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '''1.0''');
-INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '''1.2''');
-INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '''2.0''');
-INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '''3.0''');
+INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '1.0');
+INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '1.2');
+INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '2.0');
+INSERT INTO "public".service_contracts (name, version) VALUES ('test-service', '3.0');
--- judge-d-server/src/test/resources/com/hltech/judged/server/EnvironmentControllerFT.sql
+++ judge-d-server/src/test/resources/com/hltech/judged/server/EnvironmentControllerFT.sql
@@ -1,5 +1,5 @@
-INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-1', '''1.0''', 'default' ,'TEST1');
-INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-2', '''2.0''', 'default' ,'TEST1');
-INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-3', '''1.0''', 'default' ,'TEST2');
+INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-1', '1.0', 'default' ,'TEST1');
+INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-2', '2.0', 'default' ,'TEST1');
+INSERT INTO "public".service_versions (name, version, space, environment_name) VALUES ('test-service-3', '1.0', 'default' ,'TEST2');
 INSERT INTO "public".environments (name) VALUES ('TEST1');
 INSERT INTO "public".environments (name) VALUES ('TEST2');
