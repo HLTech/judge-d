921f61db4d90a419a1f4ad90c941648296d89ba3
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-08-27 19:29:03 +0200

    Removed registering JavaTimeModule implicitly according to issue fix in latest swagger-request-validator

--- judge-d-server/src/main/java/com/hltech/judged/server/config/BeanFactory.java
+++ judge-d-server/src/main/java/com/hltech/judged/server/config/BeanFactory.java
@@ -2,8 +2,6 @@ package com.hltech.judged.server.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import io.swagger.util.Json;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -13,14 +11,7 @@ public class BeanFactory {
 
     @Bean
     ObjectMapper objectMapper(Jackson2ObjectMapperBuilder mapperBuilder) {
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-
-        //According to issue https://bitbucket.org/atlassian/swagger-request-validator/issues/331/timestamp-iso8601-format-issue
-        Json.mapper().registerModule(javaTimeModule);
-
-        ObjectMapper objectMapper = mapperBuilder
-            .modules(javaTimeModule)
-            .build();
+        ObjectMapper objectMapper = mapperBuilder.build();
 
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
